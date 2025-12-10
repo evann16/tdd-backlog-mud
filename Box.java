b0a79698142bf4042e8e9f60682b9ef012d2cf2e
evann16 <evannfa@gmail.com>
2025-12-10 17:33:48 +0100

    ajout actionLook

--- Box.java
+++ Box.java
@@ -14,7 +14,32 @@ class Box{
     public boolean contains(Thing t1) {
        return this.contents.contains(t1);
     }
-    public void remove(Thing t2) {
-       this.contents.remove(t2);
+
+    public void remove(Thing t2) throws RuntimeException {
+      boolean ok =  this.contents.remove(t2);
+      if (!ok) throw new RuntimeException("Remove impossible");
+    }
+
+    private boolean open = false;
+    public void open() {
+        open = true;
+    }
+
+    public void close() {
+        open = false;
+    }
+    public Boolean isOpen() {
+        return open;
+    }
+    public String actionLook() {
+        if (!open) {
+            return "la boite est fermee";
+        }
+        ArrayList<String> liste = new ArrayList<>();
+        for (Thing t : contents) {
+            liste.add(t.name);
+        }
+        String listString = String.join(", ", liste);
+        return "la boite contient: " + listString;
     }
 }
--- TestBoxes.java
+++ TestBoxes.java
@@ -1,7 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class TestsBoxes {
+public class TestBoxes {
     @Test
     public void testBoxCreate() {
         Box b = new Box();
@@ -45,5 +45,37 @@ public class TestsBoxes {
         assertTrue(b.contains(t1));
         assertFalse(b.contains(t2));
         assertFalse(b.contains(t3));
+    }
+    @Test(expected = RuntimeException.class)
+    public void testBoxRemoveFails(){
+        Box b = new Box();
+        Thing t1 = new Thing("truc1");
+        Thing t2 = new Thing("truc2");
+        b.add(t1);
+        b.remove(t2);
+    }
+    @Test
+    public void testBoxIsOpen(){
+        Box b = new Box();
+        Thing t1 = new Thing("truc1");
+        Thing t2 = new Thing("truc2");
+        b.add(t1);
+        b.add(t2);
+        b.open();
+        assertTrue(b.isOpen());
+        b.close();
+        assertFalse(b.isOpen());
+    }
+    @Test
+    public void testBoxLook(){
+        Box b = new Box();
+        Thing t1 = new Thing("truc1");
+        Thing t2 = new Thing("truc2");
+        b.add(t1);
+        b.add(t2);
+        b.open();
+        assertEquals("la boite contient: truc1, truc2", b.actionLook());
+        b.close();
+        assertEquals("la boite est fermee", b.actionLook());
     }
 }
